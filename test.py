8619ffa368586f0d36c7c6655753e68b07668100
Egor Syrov <egor-syrov@bk.ru>
2026-04-29 09:58:13 +0300

    add alembic

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from decimal import Decimal
+from datetime import date
 
-from sqlalchemy import create_engine, Integer, String, Numeric, ForeignKey
+from sqlalchemy import Integer, String, Date, func, ForeignKey
+from sqlalchemy.schema import CreateTable
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -11,37 +12,49 @@ from sqlalchemy.orm import (
 
 
 class Base(DeclarativeBase):
+    pass
 
+
+class IdMixin:
     @declared_attr
     def id(cls) -> Mapped[int]:
         return mapped_column(Integer, primary_key=True)
 
 
-class Customer(Base):
+class Participation(Base):
+    __tablename__ = "participations"
+    project_id: Mapped[int] = mapped_column(ForeignKey("projects.id"), primary_key=True)
+    employee_id: Mapped[int] = mapped_column(
+        ForeignKey("employees.id"), primary_key=True
+    )
+    role: Mapped[str] = mapped_column(String(50), nullable=False)
 
-    __tablename__ = "customers"
+
+class Project(Base, IdMixin):
+    __tablename__ = "projects"
+
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    start_date: Mapped[date] = mapped_column(
+        Date, server_default=func.now(), nullable=False
+    )
+
+    employees: Mapped[list["Employee"]] = relationship(
+        back_populates="projects", secondary="participations"
+    )
+
+
+class Employee(Base, IdMixin):
+    __tablename__ = "employees"
 
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
-    orders: Mapped[list["Order"]] = relationship(
-        "Order", back_populates="customer", uselist=True
-    )
-
-
-class Order(Base):
-
-    __tablename__ = "orders"
-
-    order_number: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
-    total_amount: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
-    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"), nullable=False)
-
-    customer: Mapped["Customer"] = relationship(
-        "Customer", back_populates="orders", uselist=False
+    projects: Mapped[list["Project"]] = relationship(
+        back_populates="employees", secondary="participations"
     )
 
 
 if __name__ == "__main__":
-    engine = create_engine("sqlite:///:memory:", echo=True)
-    Base.metadata.create_all(engine)
+    print(CreateTable(Participation.__table__))
+    print(CreateTable(Project.__table__))
+    print(CreateTable(Employee.__table__))
